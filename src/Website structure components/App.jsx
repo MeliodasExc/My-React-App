f819b2012a4f2ac150dff4159b290e30027b717f
MeliodasExc <109146208+MeliodasExc@users.noreply.github.com>
2025-06-02 16:46:37 +0200

    deploy test2

--- src/Website structure components/App.jsx
+++ src/Website structure components/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
+import { HashRouter as Router, Routes, Route } from "react-router-dom"; 
 import Home from "../Pages/Home";
 import About from "../Pages/About";
 import Services from "../Pages/Services";
@@ -7,20 +7,16 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 export default function App() {
-
   return (
-    <>
     <Router>
-      <Header/>
+      <Header />
       <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/login" element={<Services/>} />
-      <Route path="/about-us" element={<About/>}/>
-      <Route path="/contacts" element={<Contacts/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Services />} />
+        <Route path="/about-us" element={<About />} />
+        <Route path="/contacts" element={<Contacts />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </Router>
-      </>
-    
-  )
+  );
 }
